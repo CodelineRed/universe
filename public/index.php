57e4be1a13fd1d88d58d3ad724475f37c2df8a4c
InsanityMeetsHH <software@insanitymeetshh.net>
2018-01-24 20:51:15 +0100

    Added meta theme color. Added favicon.

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@
         <title>InsanityMeetsHH Universe</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="theme-color" content="#212121">
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
         <link rel="stylesheet" href="/css/bootstrap.min.css">
         <link rel="stylesheet" href="/css/font-awesome.min.css">
         <link rel="stylesheet" href="/css/styles.css">
