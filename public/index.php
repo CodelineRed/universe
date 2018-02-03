50548d6ae78fc1326cd421a8eaca3d784086efd5
InsanityMeetsHH <software@insanitymeetshh.net>
2018-02-03 15:33:50 +0100

    Removed unnecessary classes.

--- public/index.php
+++ public/index.php
@@ -2,47 +2,48 @@
 <html>
     <head>
         <title>InsanityMeetsHH Universe</title>
+        <base href="http://www.insanitymeetshh.net/">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="theme-color" content="#212121">
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-        <link rel="stylesheet" href="/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/css/font-awesome.min.css">
-        <link rel="stylesheet" href="/css/styles.css">
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/styles.css">
     </head>
     <body>
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1 class="text-center mt-5 mb-5">InsanityMeetsHH Universe</h1>
+                    <h1 class="text-center my-5">InsanityMeetsHH Universe</h1>
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-8 offset-sm-2">
                     <div class="row">
-                        <div class="col-6 col-sm-4 text-center pt-3 pb-3">
-                            <a href="http://youtube.insanitymeetshh.net" target="_blank" class="btn btn-lg btn-link">
-                                <i class="fa fa-4x fa-youtube-play" aria-hidden="true"></i>
+                        <div class="col-6 col-sm-4 text-center py-4">
+                            <a href="http://youtube.insanitymeetshh.net" target="_blank">
+                                <i class="fa fa-5x fa-youtube-play" aria-hidden="true"></i>
                             </a>
                         </div>
-                        <div class="col-6 col-sm-4 text-center pt-3 pb-3">
-                            <a href="http://twitter.insanitymeetshh.net" target="_blank" class="btn btn-lg btn-link">
-                                <i class="fa fa-4x fa-twitter" aria-hidden="true"></i>
+                        <div class="col-6 col-sm-4 text-center py-4">
+                            <a href="http://twitter.insanitymeetshh.net" target="_blank">
+                                <i class="fa fa-5x fa-twitter" aria-hidden="true"></i>
                             </a>
                         </div>
-                        <div class="col-6 col-sm-4 text-center pt-3 pb-3">
-                            <a href="http://twitch.insanitymeetshh.net" target="_blank" class="btn btn-lg btn-link">
-                                <i class="fa fa-4x fa-twitch" aria-hidden="true"></i>
+                        <div class="col-6 col-sm-4 text-center py-4">
+                            <a href="http://twitch.insanitymeetshh.net" target="_blank">
+                                <i class="fa fa-5x fa-twitch" aria-hidden="true"></i>
                             </a>
                         </div>
-                        <div class="col-6 col-sm-4 text-center pt-3 pb-3">
-                            <a href="http://github.insanitymeetshh.net" target="_blank" class="btn btn-lg btn-link">
-                                <i class="fa fa-4x fa-github" aria-hidden="true"></i>
+                        <div class="col-6 col-sm-4 text-center py-4">
+                            <a href="http://github.insanitymeetshh.net" target="_blank">
+                                <i class="fa fa-5x fa-github" aria-hidden="true"></i>
                             </a>
                         </div>
-                        <div class="col-6 col-sm-4 text-center pt-3 pb-3">
-                            <a href="http://packagist.insanitymeetshh.net" target="_blank" class="btn btn-lg btn-link">
-                                <i class="fa fa-4x fa-dropbox" aria-hidden="true"></i>
+                        <div class="col-6 col-sm-4 text-center py-4">
+                            <a href="http://packagist.insanitymeetshh.net" target="_blank">
+                                <i class="fa fa-5x fa-dropbox" aria-hidden="true"></i>
                             </a>
                         </div>
                     </div>
