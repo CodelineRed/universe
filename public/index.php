1152b1550c8bcaea336416299f780171db813b25
InsanityMeetsHH <software@insanitymeetshh.net>
2018-04-19 20:42:07 +0200

    Upgrade to Fontawesome 5.

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
         <title>InsanityMeetsHH Universe</title>
         <base href="http://www.insanitymeetshh.net/">
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         <meta name="theme-color" content="#212121">
         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
         <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -21,37 +21,37 @@
                     <div class="row">
                         <div class="col-6 col-sm-4 text-center py-4">
                             <a href="http://youtube.insanitymeetshh.net" target="_blank">
-                                <i class="fa fa-5x fa-youtube-play" aria-hidden="true"></i>
+                                <i class="fab fa-5x fa-youtube" aria-hidden="true"></i>
                                 <div class="pt-2">YouTube</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
                             <a href="http://twitter.insanitymeetshh.net" target="_blank">
-                                <i class="fa fa-5x fa-twitter" aria-hidden="true"></i>
+                                <i class="fab fa-5x fa-twitter" aria-hidden="true"></i>
                                 <div class="pt-2">Twitter</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
                             <a href="http://twitch.insanitymeetshh.net" target="_blank">
-                                <i class="fa fa-5x fa-twitch" aria-hidden="true"></i>
+                                <i class="fab fa-5x fa-twitch" aria-hidden="true"></i>
                                 <div class="pt-2">Twitch</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
                             <a href="http://github.insanitymeetshh.net" target="_blank">
-                                <i class="fa fa-5x fa-github" aria-hidden="true"></i>
+                                <i class="fab fa-5x fa-github" aria-hidden="true"></i>
                                 <div class="pt-2">GitHub</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
                             <a href="http://packagist.insanitymeetshh.net" target="_blank">
-                                <i class="fa fa-5x fa-dropbox" aria-hidden="true"></i>
+                                <i class="fab fa-5x fa-dropbox" aria-hidden="true"></i>
                                 <div class="pt-2">Packagist</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
                             <a href="http://steam.insanitymeetshh.net" target="_blank">
-                                <i class="fa fa-5x fa-steam" aria-hidden="true"></i>
+                                <i class="fab fa-5x fa-steam-symbol" aria-hidden="true"></i>
                                 <div class="pt-2">Steam</div>
                             </a>
                         </div>
@@ -66,25 +66,25 @@
                     <div class="row">
                         <div class="col-6 col-sm-4 text-center py-4">
                             <a href="http://ca.insanitymeetshh.net/" target="_blank">
-                                <i class="fa fa-5x fa-css3" aria-hidden="true"></i>
+                                <i class="fab fa-5x fa-css3" aria-hidden="true"></i>
                                 <div class="pt-2">Canvas Animation</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
                             <a href="http://fa47.insanitymeetshh.net/" target="_blank">
-                                <i class="fa fa-5x fa-font-awesome" aria-hidden="true"></i>
+                                <i class="fab fa-5x fa-font-awesome-alt" aria-hidden="true"></i>
                                 <div class="pt-2">FontAwesome 4.7 CheatSheet</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
                             <a href="http://gulp.insanitymeetshh.net/" target="_blank">
-                                <i class="fa fa-5x fa-terminal" aria-hidden="true"></i>
+                                <i class="fas fa-5x fa-terminal" aria-hidden="true"></i>
                                 <div class="pt-2">Gulp Templating</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
                             <a href="http://slim3.insanitymeetshh.net/" target="_blank">
-                                <i class="fa fa-5x fa-code-fork" aria-hidden="true"></i>
+                                <i class="fas fa-5x fa-code-branch" aria-hidden="true"></i>
                                 <div class="pt-2">Slim Framework 3 Skeleton (Fork)</div>
                             </a>
                         </div>
@@ -99,7 +99,7 @@
                     <div class="row">
                         <div class="col-12 text-center py-4">
                             <a href="http://stream.insanitymeetshh.net/" target="_blank">
-                                <i class="fa fa-5x fa-gamepad" aria-hidden="true"></i>
+                                <i class="fas fa-5x fa-gamepad" aria-hidden="true"></i>
                                 <div class="pt-2">Stream Information</div>
                             </a>
                         </div>
