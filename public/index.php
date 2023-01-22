b20284c1cdfe0a9f1e209519e976e6b4664778f5
CodelineRed <software@codelinered.net>
2023-01-22 17:35:58 +0100

    Added aria label
    Fixed data-text value by Twitch Chatbot & Code Snippets

--- public/index.php
+++ public/index.php
@@ -152,55 +152,55 @@ $meta = array(
                 <div class="col-sm-8 offset-sm-2">
                     <div class="row">
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="http://twitter.codelinered.net" target="_blank">
+                            <a href="http://twitter.codelinered.net" target="_blank" aria-label="CodelineRed on Twitter">
                                 <i class="fab fa-5x fa-twitter" aria-hidden="true"></i>
                                 <div class="mt-2 glitch-text" data-text="Twitter">Twitter</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="http://instagram.codelinered.net" target="_blank">
+                            <a href="http://instagram.codelinered.net" target="_blank" aria-label="CodelineRed on Instagram">
                                 <i class="fab fa-5x fa-instagram" aria-hidden="true"></i>
                                 <div class="mt-2 glitch-text" data-text="Instagram">Instagram</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="http://twitch.codelinered.net" target="_blank">
+                            <a href="http://twitch.codelinered.net" target="_blank" aria-label="CodelineRed on Twitch">
                                 <i class="fab fa-5x fa-twitch" aria-hidden="true"></i>
                                 <div class="mt-2 glitch-text" data-text="Twitch">Twitch</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="http://youtube.codelinered.net" target="_blank">
+                            <a href="http://youtube.codelinered.net" target="_blank" aria-label="CodelineRed on YouTube">
                                 <i class="fab fa-5x fa-youtube" aria-hidden="true"></i>
                                 <div class="mt-2 glitch-text" data-text="YouTube">YouTube</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="http://github.codelinered.net" target="_blank">
+                            <a href="http://github.codelinered.net" target="_blank" aria-label="CodelineRed on GitHub">
                                 <i class="fab fa-5x fa-github custom" aria-hidden="true"></i>
                                 <div class="mt-2 glitch-text" data-text="GitHub">GitHub</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="http://packagist.codelinered.net" target="_blank">
+                            <a href="http://packagist.codelinered.net" target="_blank" aria-label="CodelineRed on Packagist">
                                 <i class="fab fa-5x fa-dropbox" aria-hidden="true"></i>
                                 <div class="mt-2 glitch-text" data-text="Packagist">Packagist</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="http://npm.codelinered.net" target="_blank">
+                            <a href="http://npm.codelinered.net" target="_blank" aria-label="CodelineRed on NPM">
                                 <i class="fab fa-5x fa-npm" aria-hidden="true"></i>
                                 <div class="mt-2 glitch-text" data-text="NPM">NPM</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="http://steam.codelinered.net" target="_blank">
+                            <a href="http://steam.codelinered.net" target="_blank" aria-label="CodelineRed on Steam">
                                 <i class="fab fa-5x fa-steam-symbol" aria-hidden="true"></i>
                                 <div class="mt-2 glitch-text" data-text="Steam">Steam</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="http://stackoverflow.codelinered.net/" target="_blank">
+                            <a href="http://stackoverflow.codelinered.net/" target="_blank" aria-label="CodelineRed on Stackoverflow">
                                 <i class="fab fa-5x fa-stack-overflow" aria-hidden="true"></i>
                                 <div class="mt-2 glitch-text" data-text="Stackoverflow">Stackoverflow</div>
                             </a>
@@ -228,7 +228,7 @@ $meta = array(
                                 <meta itemprop="price" content="0">
                                 <meta itemprop="priceCurrency" content="EUR">
                             </div>
-                            <a href="https://slim3.codelinered.net" target="_blank">
+                            <a href="https://slim3.codelinered.net" target="_blank" aria-label="Slim Skeleton demo page">
                                 <span class="fa-layers fa-5x">
                                     <i class="fas fa-box"></i>
                                     <i class="fab fa-php" data-fa-transform="shrink-5.5 down-2.5 left-2" style="color:#212121;"></i>
@@ -249,7 +249,7 @@ $meta = array(
                                 <meta itemprop="price" content="0">
                                 <meta itemprop="priceCurrency" content="EUR">
                             </div>
-                            <a href="https://gulp.codelinered.net" target="_blank">
+                            <a href="https://gulp.codelinered.net" target="_blank" aria-label="Gulp Skeleton demo page">
                                 <i class="fab fa-5x fa-gulp" aria-hidden="true"></i>
                                 <span itemprop="name" class="mt-2 d-block glitch-text" data-text="Gulp Skeleton">Gulp Skeleton</span>
                             </a>
@@ -268,7 +268,7 @@ $meta = array(
                                 <meta itemprop="price" content="0">
                                 <meta itemprop="priceCurrency" content="EUR">
                             </div>
-                            <a href="https://vue.codelinered.net" target="_blank">
+                            <a href="https://vue.codelinered.net" target="_blank" aria-label="Vue Skeleton demo page">
                                 <i class="fab fa-5x fa-vuejs" aria-hidden="true"></i>
                                 <span itemprop="name" class="mt-2 d-block glitch-text" data-text="Vue Skeleton">Vue Skeleton</span>
                             </a>
@@ -287,7 +287,7 @@ $meta = array(
                                 <meta itemprop="price" content="0" />
                                 <meta itemprop="priceCurrency" content="EUR" />
                             </div>
-                            <a href="https://packagist.org/packages/codelinered/file-sharing" target="_blank">
+                            <a href="https://packagist.org/packages/codelinered/file-sharing" target="_blank" aria-label="File Sharing on Packagist">
                                 <span class="fa-layers fa-5x">
                                     <i class="fas fa-share-alt" data-fa-mask="fas fa-file" data-fa-transform="shrink-7 down-2.5"></i>
                                     <i class="fab fa-php" data-fa-transform="shrink-11 up-5.5 left-4.5" style="color:#212121;"></i>
@@ -309,12 +309,12 @@ $meta = array(
                                 <meta itemprop="price" content="0" />
                                 <meta itemprop="priceCurrency" content="EUR" />
                             </div>
-                            <a href="https://github.com/CodelineRed/twitch-chatbot" target="_blank">
+                            <a href="https://github.com/CodelineRed/twitch-chatbot" target="_blank" aria-label="Twitch Chatbot on GitHub">
                                 <span class="fa-layers fa-5x">
                                     <i class="fab fa-twitch" style="color:#c9c9c9;"></i>
                                     <i class="fas fa-robot" data-fa-transform="shrink-12 down-5.5 right-4.5"></i>
                                 </span>
-                                <span itemprop="name" class="mt-2 d-block glitch-text" data-text="File Sharing">Twitch Chatbot</span>
+                                <span itemprop="name" class="mt-2 d-block glitch-text" data-text="Twitch Chatbot">Twitch Chatbot</span>
                             </a>
                         </div>
                         <div itemscope itemtype="http://schema.org/SoftwareSourceCode" class="col-6 col-sm-4 text-center py-4">
@@ -329,7 +329,7 @@ $meta = array(
                                 <meta itemprop="price" content="0">
                                 <meta itemprop="priceCurrency" content="EUR">
                             </div>
-                            <a href="https://ca.codelinered.net" target="_blank">
+                            <a href="https://ca.codelinered.net" target="_blank" aria-label="Canvas Animation demo page">
                                 <span class="fa-layers fa-5x">
                                     <i class="fas fa-puzzle-piece"></i>
                                     <span class="fa-layers-text" data-fa-transform="shrink-13 down-0.5 rotate-45" style="font-weight:900">jQuery</span>
@@ -349,7 +349,7 @@ $meta = array(
                                 <meta itemprop="price" content="0" />
                                 <meta itemprop="priceCurrency" content="EUR" />
                             </div>
-                            <a href="https://www.npmjs.com/package/bootstrap-breakpoint" target="_blank">
+                            <a href="https://www.npmjs.com/package/bootstrap-breakpoint" target="_blank" aria-label="Bootstrap Breakpoint on NPM">
                                 <span class="fa-layers fa-5x">
                                     <i class="fas fa-puzzle-piece"></i>
                                     <span class="fa-layers-text" data-fa-transform="shrink-13.75 left-1 down-0.5 rotate-45" style="font-weight:900">JavaScript</span>
@@ -371,7 +371,7 @@ $meta = array(
                                 <meta itemprop="price" content="0" />
                                 <meta itemprop="priceCurrency" content="EUR" />
                             </div>
-                            <a href="https://github.com/CodelineRed/typo3-skeleton" target="_blank">
+                            <a href="https://github.com/CodelineRed/typo3-skeleton" target="_blank" aria-label="TYPO3 8.7 Skeleton on GitHub">
                                 <i class="fab fa-5x fa-typo3" aria-hidden="true"></i>
                                 <span itemprop="name" class="mt-2 d-block glitch-text" data-text="TYPO3 8.7 Skeleton (DEV)">TYPO3 8.7 Skeleton (DEV)</span>
                             </a>
@@ -391,7 +391,7 @@ $meta = array(
                                 <meta itemprop="price" content="0" />
                                 <meta itemprop="priceCurrency" content="EUR" />
                             </div>
-                            <a href="https://github.com/CodelineRed/typo3-base-ext" target="_blank">
+                            <a href="https://github.com/CodelineRed/typo3-base-ext" target="_blank" aria-label="TYPO3 Base Extension on GitHub">
                                 <span class="fa-layers fa-5x">
                                     <i class="fas fa-puzzle-piece"></i>
                                     <span class="fa-layers-text" data-fa-transform="shrink-13 left-0.75 down-0.5 rotate-45" style="font-weight:900">TYPO3</span>
@@ -400,9 +400,9 @@ $meta = array(
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="http://gist.codelinered.net" target="_blank">
+                            <a href="http://gist.codelinered.net" target="_blank" aria-label="Code Snippets on Gist">
                                 <i class="fab fa-5x fa-github" aria-hidden="true"></i>
-                                <div class="mt-2 glitch-text" data-text="GitHub Gist">Code Snippets</div>
+                                <div class="mt-2 glitch-text" data-text="Code Snippets">Code Snippets</div>
                             </a>
                         </div>
                     </div>
@@ -415,13 +415,13 @@ $meta = array(
                 <div class="col-sm-8 offset-sm-2">
                     <div class="row">
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="https://stream.codelinered.net/" target="_blank">
+                            <a href="https://stream.codelinered.net/" target="_blank" aria-label="Stream Information">
                                 <i class="fas fa-5x fa-gamepad" aria-hidden="true"></i>
                                 <div class="mt-2 glitch-text" data-text="Stream Information">Stream Information</div>
                             </a>
                         </div>
                         <div class="col-6 col-sm-4 text-center py-4">
-                            <a href="https://devbook.codelinered.net/" target="_blank">
+                            <a href="https://devbook.codelinered.net/" target="_blank" aria-label="Developer Bookmark">
                                 <i class="fas fa-5x fa-book" aria-hidden="true"></i>
                                 <div class="mt-2 glitch-text" data-text="Developer Bookmark">Developer Bookmark</div>
                             </a>
